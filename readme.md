2bd8445afeb8ec24089a0b36b47c501cd1b26045
Leo <leonoragilmour@sky.com>
2017-07-17 12:23:07 +0100

    init

---
+++ readme.md
@@ -0,0 +1,2 @@
+# WP Header Signup Plugin
+A pluging to add signup fields to the header
---
+++ wp-header-signup-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: WP Header Signup Plugin
+* Plugin URI: http://github.com/shortlist-digital/wp-header-signup-plugin
+* Description: A pluging to add signup fields to the header
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+class WpHeaderSignupPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new WpHeaderSignupPlugin();
